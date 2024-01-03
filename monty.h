0dddc773431abaf95745c83528eb9f448e671257
Arweenn <arwendumont25@outlook.fr>
2024-01-03 13:42:36 +0100

    adding swap function

--- monty.h
+++ monty.h
@@ -40,6 +40,7 @@ int is_valid_int(const char *str);
 instruction_t parse_line(char *line, unsigned int line_number);
 void pint(stack_t **stack, unsigned int line_number);
 void pop(stack_t **stack, unsigned int line_number);
+void swap(stack_t **stack, unsigned int line_number);
 
 #endif /* MONTY_H */
 
