10376bd22f463cf55c8eb7967ba3923a9c22e7b1
Arweenn <arwendumont25@outlook.fr>
2024-01-04 13:53:49 +0100

    output message fixed

--- adv_sub.c
+++ adv_sub.c
@@ -10,7 +10,7 @@ void sub(stack_t **stack, unsigned int line_number)
 {
 	if (*stack == NULL || (*stack)->next == NULL)
 	{
-		fprintf(stderr, "L%u: can't add, stack too short\n", line_number);
+		fprintf(stderr, "L%u: can't sub, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
