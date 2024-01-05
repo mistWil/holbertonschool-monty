ac119a1f61388448895b786834085de2b4bcdcb3
Arweenn <arwendumont25@outlook.fr>
2024-01-05 16:15:00 +0100

    mul opcode implemented

---
+++ adv_mul.c
@@ -0,0 +1,19 @@
+#include "monty.h"
+
+/**
+ * mul - Implements the mul opcode
+ * @stack: The stack
+ * @line_number: Line number in the file
+ */
+
+void mul(stack_t **stack, unsigned int line_number)
+{
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%u: can't mul, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	(*stack)->next->n *= (*stack)->n;
+	pop(stack, line_number);
+}
--- monty.h
+++ monty.h
@@ -46,5 +46,6 @@ void add(stack_t **stack, unsigned int line_number);
 void nop(stack_t **stack, unsigned int line_number);
 
 void sub(stack_t **stack, unsigned int line_number);
+void mul(stack_t **stack, unsigned int line_number);
 
 #endif /* MONTY_H */
