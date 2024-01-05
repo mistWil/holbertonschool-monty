e7aebefb70d151fba8b1f93be0abb911d68294b3
Arweenn <arwendumont25@outlook.fr>
2024-01-05 15:53:54 +0100

    space fix

--- main_monty.c
+++ main_monty.c
@@ -17,29 +17,39 @@ int main(int argc, char *argv[])
 	stack_t *stack = NULL;
 	instruction_t instruction;
 
-	if (argc != 2)
-	{
-		fprintf(stderr, "USAGE: monty file\n");
-		exit(EXIT_FAILURE);
-	}
+int main(int argc, char *argv[])
+{
+    FILE *file;
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t read;
+    unsigned int line_number = 0;
+    stack_t *stack = NULL;
+    instruction_t instruction;
 
-	file = fopen(argv[1], "r");
-	if (!file)
-	{
-		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-		exit(EXIT_FAILURE);
-	}
+    if (argc != 2)
+    {
+        fprintf(stderr, "USAGE: monty file\n");
+        exit(EXIT_FAILURE);
+    }
 
-	while ((read = getline(&line, &len, file)) != -1)
-	{
-		line_number++;
-		instruction = parse_line(line, line_number);
-		if (instruction.f)
-			instruction.f(&stack, line_number);
-	}
+    file = fopen(argv[1], "r");
+    if (!file)
+    {
+        fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+        exit(EXIT_FAILURE);
+    }
 
-	free(line);
-	free_stack(stack);
-	fclose(file);
-	return (EXIT_SUCCESS);
+    while ((read = getline(&line, &len, file)) != -1)
+    {
+        line_number++;
+        instruction = parse_line(line, line_number);
+        if (instruction.f)
+            instruction.f(&stack, line_number);
+    }
+
+    free(line);
+    free_stack(stack);
+    fclose(file);
+    return (EXIT_SUCCESS);
 }
